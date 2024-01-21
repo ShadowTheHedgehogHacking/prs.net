ddea2df9ae19bb4d7fae217a1896f1396c42c15f
dreamsyntax <dreamsyntax@gmail.com>
2024-01-21 16:41:30 -0700

    .net standard 2.0 add langversion

--- FraGag.Compression.Prs/Properties/AssemblyInfo.cs
+++ FraGag.Compression.Prs/Properties/AssemblyInfo.cs
@@ -2,16 +2,6 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Resources;
-
-// Les informations générales relatives à un assembly dépendent de 
-// l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
-// associées à un assembly.
-[assembly: AssemblyTitle("FraGag.Compression.Prs")]
-[assembly: AssemblyDescription("PRS compression/decompression library")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Francis Gagné")]
-[assembly: AssemblyProduct("FraGag.Compression.Prs")]
-[assembly: AssemblyCopyright("Copyright © Francis Gagné 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -22,17 +12,3 @@ using System.Resources;
 
 // Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
 [assembly: Guid("f4cb71ab-cb68-4162-8fe5-e239aed35e20")]
-
-// Les informations de version pour un assembly se composent des quatre valeurs suivantes :
-//
-//      Version principale
-//      Version secondaire 
-//      Numéro de build
-//      Révision
-//
-// Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut 
-// en utilisant '*', comme indiqué ci-dessous :
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: NeutralResourcesLanguageAttribute("en-US")]
--- README.md
+++ README.md
@@ -1,8 +1,3 @@
-# PRS compression/decompression library for the .NET Framework
+# C# PRS compression/decompression library 
 
-This is a port of [fuzziqer's PRS compression/decompression utility](https://github.com/essen/prsutil) as a library targetting the Microsoft .NET Framework, version 2.0 or later.
-
-The PRS compression algorithm is based on LZ77 with run-length encoding
-emulation and extended matches. It is used in numerous games since the
-introduction of the SEGA Saturn, including Phantasy Star Universe inside
-the nbl file format.
+This is a fork of [FraGag's prs.net](https://github.com/FraGag/prs.net) as a library targetting .NET Standard 2.0
